2b5de3012dc87fa2e1d690358e08e9ed3865dab0
Pratham Agrawal <prathamagrawal179@gmail.com>
2024-08-31 11:46:18 +0530

    Search by keyword

--- src/main/java/com/pratham/spring_boot_rest/repo/JobRepo.java
+++ src/main/java/com/pratham/spring_boot_rest/repo/JobRepo.java
@@ -4,10 +4,14 @@ import com.pratham.spring_boot_rest.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
 public  interface JobRepo extends JpaRepository<JobPost,Integer> {
+
+    List<JobPost> findByPostProfileContainingOrPostDescContainingIgnoreCase(String postProfile, String postDesc);
+
 }
 
 
